75068c142ca8140fe11bf10959ad4775204926fa
Louis Roehrs <lroehrs@gmail.com>
2023-02-27 18:52:11 -0800

    Updated with marketing graphics

---
+++ lineup.js
@@ -0,0 +1,37 @@
+
+
+
+
+// Setup the lightening talk just by filling out these variables and save the file.
+// This is the only file needed, all assets in this HTML.
+// Point your browser to index.html.
+// 
+// Add a talk and save, the file will automatically update on its own schedule.  1/3 minute or just reload.
+
+// Save this file to our storage place (TBD) to keel a record. Use this filename convention.  LighteningTalk-<talkdate>.html
+      
+talks = [
+  "Hacks from a Bootstrapping Solopreneur - Natalia",
+  "Explainable AI - Masha",
+  "Just Ski - Lena",
+  "Something Cool - Strick",
+  "Wisdom - Roy",
+  "Garbage Classification - Ravendu",
+  "JetRacer Controlled by Body Posture - Rashika",
+  "Coffeespace - Hazim",
+  "Take Great Care of Yourself - Kevin",
+  "Boundary App - Jim",
+];
+
+hosts = "Host and Ishai";
+
+engage = [
+  "Become a member https://hackerdojo.com",
+  "Give at https://givebutter.com/growhackerdojo",
+  "Attend events https://www.meetup.com/hackerdojo",
+  "Watch, like, and subscribe https://www.youtube.com/HackerDojo",
+];
+
+qrcodeurl = "https://givebutter.com/growhackerdojo"
+
+
