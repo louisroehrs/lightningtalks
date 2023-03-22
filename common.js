3dfb437ee198ba447698f8a33dd20600b1fe0639
Louis Roehrs <lroehrs@gmail.com>
2023-03-22 12:34:17 -0700

    Refactoring javascript to common.js and added memberbenefits.html screen.

---
+++ common.js
@@ -0,0 +1,12 @@
+let qrcodeurl = "https://givebutter.com/growhackerdojo";
+
+let pages =  {
+  "index" : "./index.html?alt=media&token=9151d217-caf6-44d5-8066-70d3b45b3654",
+  "agenda" : "./agenda.html?alt=media&token=c94eadbd-65f8-4817-a4df-3e321612d07d",
+  "after" : "./after.html?alt=media&token=59344ded-8cbb-4e39-b3e6-72719c436c9e",
+  "welcome": "./welcome.html?alt=media&token=6f22f023-5615-4acb-9a86-c6dda182a24c",
+  "pixel" : "./pixel.html?alt=media&token=ac1bbe00-a0f5-4f5d-8603-3c6231eaa38f",
+  "support" :"./support.html?alt=media&token=4eedd9bd-0817-4685-a9fe-eef17d11c551",
+  "benefitbutton" : "./memberbenefits.html?alt=media&token=d6f63997-ae16-49bf-bb56-23e3810a5675"
+};
+
