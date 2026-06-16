43c6c81ff5a0c41a9e3c359981c5aba229ba6941
Louis Roehrs <lroehrs@gmail.com>
2026-06-16 00:17:55 -0700

    Refactors styles and javascript. Edit lineup and push to change

---
+++ talks.js
@@ -0,0 +1,92 @@
+
+var data = null;
+
+function generateQR(text) {
+  const canvas = document.getElementById("qrcodecanvas");
+  QRCode.toCanvas(canvas, text, {
+    width: 256,
+    margin: 2
+  }, function (error) {
+    if (error) {
+      console.error(error);
+      alert("Failed to generate QR code");
+    }
+  });
+}
+
+function listTalks(talks) {
+  document.getElementById("talks").innerHTML ="";
+  talks.map( (talk,i) => {
+    if (talks[i].trim().length > 0) {
+      const li = document.createElement("li");
+      li.innerText = talks[i];
+      document.getElementById("talks").append(li);
+    }
+  })
+};
+
+function fetchJSON() {
+  // fetch("lineup.json")
+  fetch("https://raw.githubusercontent.com/louisroehrs/lightningtalks/refs/heads/main/lineup.json")
+    .then(response => response.json())
+    .then(respdata => {
+      data = respdata;
+      debugger;
+      if (!location.href.includes(data.showpage)) {
+        location.href = data.pages[data.showpage];
+      }
+      console.log(data);
+      if ( document.getElementById("hosts")) {
+        document.getElementById("hosts").innerText = data.hosts;
+      }
+      if ( document.getElementById("talks")) {
+        listTalks(data.talks);
+      }
+      if ( document.getElementById("qrcodecanvas")) {
+        generateQR(document.getElementById("qrcodetext").getHTML());
+      }
+    })
+    .catch(error => {
+      if ( document.getElementById("talks")) {
+        document.getElementById("talks").innerText = "Check fetching lineup";
+      }
+      console.error(error);
+    });
+}
+
+// fetch JSON initially
+fetchJSON();
+
+// reload JSON every 20 seconds
+setInterval(fetchJSON, 20000);
+
+function updateTime() {
+  if ( !document.getElementById("currentTime")) {
+    return;
+  }
+  
+  const eventStart = "19:00";
+  const s = eventStart.split(':');
+  const startTime = new Date();
+  startTime.setHours(s[0]);
+  startTime.setMinutes(s[1]);
+  
+  const now = new Date();
+  
+  timeLeft = startTime - now;
+  
+  minutes =  Math.ceil(timeLeft / 1000 / 60);
+  
+  document.getElementById("currentTime").innerText = minutes  + "minutes";
+  document.getElementById("talkdate").innerText = now.toDateString();
+  
+  if ( minutes > 0) {
+    
+    document.getElementById("countdown").innerText=" in " + minutes + " minutes <BR>at 7:00pm";
+    setTimeout(updateTime,1000);
+  } else {
+    document.getElementById("countdown").innerHTML ="ing now <BR>";
+  }
+}
+
+setTimeout(updateTime, 1);
